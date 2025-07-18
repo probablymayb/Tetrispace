c7a3fb6a777a89e6ebd090dc9929c4ac532a99e0
Donghee <luz0415@naver.com>
2025-07-19 00:49:23 +0900

    Feat | 플레이어 총알 발사 구현

---
+++ Tetrispace/Assets/Scripts/03. Player/PlayerShooter.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooter : MonoBehaviour
+{
+    private InputAction shootAction;
+    private const string ShootActionName = "Attack";
+    
+    [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float attackCooldown = 0.3f;
+    [SerializeField] private BulletData bulletData;
+
+    private float attackTimer = 0f;
+    private bool isAttackHeld = false;
+    
+    private void Awake()
+    {
+        shootAction = InputSystem.actions.FindAction(ShootActionName);
+        PoolManager.Instance.CreatePool(bulletPrefab);
+    }
+
+    private void OnEnable()
+    {
+        shootAction.Enable();
+        shootAction.started += OnAttack;
+        shootAction.performed += OnAttack;
+        shootAction.canceled += OnAttack;
+    }
+
+    private void OnDisable()
+    {
+        shootAction.started -= OnAttack;
+        shootAction.performed -= OnAttack;
+        shootAction.canceled -= OnAttack;
+        shootAction.Disable();
+    }
+
+    private void Update()
+    {
+        if (isAttackHeld)
+        {
+            attackTimer -= Time.deltaTime;
+
+            if (attackTimer <= 0f)
+            {
+                Fire();
+                attackTimer = attackCooldown;
+            }
+        }
+    }
+
+    private void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (attackTimer <= 0f)
+            {
+                Fire();
+                attackTimer = attackCooldown;
+            }
+            isAttackHeld = true;
+        }
+        else if (context.canceled)
+        {
+            isAttackHeld = false;
+        }
+    }
+    
+    private void Fire()
+    {
+        Bullet pooledBullet = PoolManager.Instance.Get(bulletPrefab);
+        pooledBullet.transform.position = firePoint.position;
+        pooledBullet.transform.rotation = Quaternion.identity;
+
+        pooledBullet.Setup(bulletData);
+        pooledBullet.Fire(Vector2.up);
+    }
+}
