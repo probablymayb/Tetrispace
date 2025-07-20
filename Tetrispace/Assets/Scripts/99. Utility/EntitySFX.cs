c2994b4e6869adec48c8ef31edef133d7a9b0d88
probablymayb <proveprove@naver.com>
2025-07-20 09:27:42 +0900

    [FEAT] 사운드 추가

--- Tetrispace/Assets/Scripts/99. Utility/EntitySFX.cs
+++ Tetrispace/Assets/Scripts/99. Utility/EntitySFX.cs
@@ -3,19 +3,11 @@ using UnityEngine;
 
 public class EntitySFX : MonoBehaviour
 {
-   // [Header("FMOD Events")]
-    [SerializeField]
-   // private EventReference footstepSound;
-
-    private void PlayFootstepSound()
+    // [Header("FMOD Events")]
+    [SerializeField] AudioClip BGM;
+    // [Header("FMOD Events")]
+    private void Awake()
     {
-        //if (footstepSound.IsNull)
-        //{
-        //    Debug.LogWarning("footstep 사운드 이벤트를 찾을 수 없음.");
-        //}
-        //else
-        //{
-        //    //AudioManager.Instance.PlayOneShot(footstepSound, transform.position);
-        //}
+        SFXManager.Instance.PlayBGM(BGM, 1F, 1F);
     }
 }
---
+++ Tetrispace/Assets/Sounds/SFX/readme.txt
@@ -0,0 +1,12 @@
+opening: 초반 컷신 연출
+
+menu_swap: 타이틀, 뱀서 강화 요소 메뉴 이동(Select 6)
+
+menu_select: 타이틀 메뉴 선택 
+power_select: 뱀서 강화 선택
+
+enemy_dead: 적 사망
+user_hit: 플레이어 피격
+user_keyQ&E_hit: Q, E 탄환이 테트리스 블록에 충돌시 재생 Q 키 기본 / E 키 피치 상승 변형
+user_keyW: W 키 입력시 1회 재생
+block_erase: 테트리스 1Line 제거시 재생
